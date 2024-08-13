d2e18e7acb4170bfecc58ea9b280194abf6b5c1e
An <loc.duc.an@linecorp.com>
2024-08-13 15:13:19 +0700

    refactor: Update element ID in index.html and main.tsx for image converter

--- src/converter-worker2.ts
+++ src/converter-worker2.ts
@@ -2,21 +2,27 @@ import {
   initializeImageMagick,
   ImageMagick,
   MagickFormat,
+  MagickImageCollection,
 } from "@imagemagick/magick-wasm"
 
 // globalThis.Buffer = Buffer
-export async function convert(file: Uint8Array, targetFormat: MagickFormat) {
+export async function convert(file: Uint8Array, targetFormat: MagickFormat, quanlity: number) {
   await fetch(new URL("@imagemagick/magick-wasm/magick.wasm?wasm", import.meta.url))
     .then((res) => res.arrayBuffer())
     .then((wasmBytes) => initializeImageMagick(wasmBytes))
 
   return new Promise<ArrayBufferLike>(async (resolve, reject) => {
-    ImageMagick.read(file, (image) => {
-      image.quality = 85;
-      // image.strip()
-      image.write(targetFormat, (data) => {
-        resolve(data)
+    try {
+      ImageMagick.readCollection(file, (image) => {
+        // image.quality = quanlity;
+        // image.strip()
+        image.coalesce()
+        image.write(targetFormat, (data) => {
+          resolve(data)
+        })
       })
-    })
+    } catch(e) {
+      reject(e)
+    }
   })
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client"
 import App from "./App"
 import "./App.css"
 
-const container = document.getElementById("root")!
+const container = document.getElementById("image-converter")!
 const root = createRoot(container)
 
 root.render(
