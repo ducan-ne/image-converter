e6864bbdfa7b6b43ca916ef73ffa3aad4357d924
An <16853175+ducan-ne@users.noreply.github.com>
2024-08-24 21:19:13 +0700

    refactor: Optimize image processing and update dependencies in converter-worker2.ts

--- src/Converter.tsx
+++ src/Converter.tsx
@@ -4,11 +4,13 @@ import {
   Button,
   Cell,
   Column,
+  DropZone,
   FileTrigger,
   Row,
   Table,
   TableBody,
   TableHeader,
+  type FileDropItem,
 } from "react-aria-components"
 import { Toaster } from "sonner"
 import { MagickFormat } from "@imagemagick/magick-wasm"
@@ -75,96 +77,107 @@ export type Formats =
 
 const Converter = () => {
   const { convertedImages } = useSnapshot(state)
-  const [targetFormat, setTargetFormat] = useState<MagickFormat>(MagickFormat.Jpeg)
+  const [targetFormat, setTargetFormat] = useState<MagickFormat>(MagickFormat.Png)
   const [quanlity, setQuanlity] = useState(85)
+
+  const onFiles = (files: File[]) => {
+    for (const file of files) {
+      if (file.type.indexOf("image") === -1) {
+        continue
+      }
+      ;(async function () {
+        const instance = WorkaroundWorker()
+        const index = state.convertedImages.push({
+          status: "loading",
+          originalSize: file.size,
+          previewUrl: URL.createObjectURL(file),
+          convertedSize: 0,
+          sizeChange: 0,
+          format: targetFormat,
+          duration: 0,
+          downloadUrl: "",
+          filename: file.name,
+        })
+        const start = Date.now()
+        const timer = setInterval(() => {
+          if (state.convertedImages[index - 1].status !== "loading") {
+            clearInterval(timer)
+            return
+          }
+          state.convertedImages[index - 1].duration = Date.now() - start
+        }, 300)
+        try {
+          const converted = await instance.convert(
+            new Uint8Array(await file.arrayBuffer()),
+            targetFormat,
+            quanlity,
+          )
+
+          let blobType
+
+          if (targetFormat === MagickFormat.Pdf) {
+            blobType = "application/pdf"
+          } else if (targetFormat === MagickFormat.Psd) {
+            blobType = "application/psd"
+          } else if (targetFormat === MagickFormat.WebP) {
+            blobType = "image/webp"
+          }
+
+          const blob = new Blob([converted], {
+            type: blobType,
+          })
+          const previewUrl = URL.createObjectURL(blob)
+          // state.convertedImages[index - 1].previewUrl = previewUrl
+          state.convertedImages[index - 1].previewUrl = previewUrl
+          state.convertedImages[index - 1].convertedSize = blob.size
+          state.convertedImages[index - 1].downloadUrl = URL.createObjectURL(blob)
+          state.convertedImages[index - 1].sizeChange = ((file.size - blob.size) / file.size) * 100
+          state.convertedImages[index - 1].status = "done"
+          state.convertedImages[index - 1].format = targetFormat
+          state.convertedImages[index - 1].duration = Date.now() - start
+        } catch (e) {
+          console.log("Error:", e)
+          state.convertedImages[index - 1].status = "error"
+        }
+      })()
+    }
+  }
 
   return (
     <section id="image-converter" style={{ width: "100%", height: "100%" }}>
       <div className="flex gap-4 flex-col items-center light">
         <Toaster position="top-center" />
-        <FileTrigger
-          allowsMultiple
-          // acceptDirectory
-          onSelect={async (e) => {
-            if (!e) {
-              return
-            }
-            let files = Array.from(e)
-            if (files.length === 0) {
-              return
-            }
-            for (const file of files) {
-              if (file.type.indexOf("image") === -1) {
-                continue
-              }
-              ;(async function () {
-                const instance = WorkaroundWorker()
-                const index = state.convertedImages.push({
-                  status: "loading",
-                  originalSize: file.size,
-                  previewUrl: URL.createObjectURL(file),
-                  convertedSize: 0,
-                  sizeChange: 0,
-                  format: targetFormat,
-                  duration: 0,
-                  downloadUrl: "",
-                  filename: file.name,
-                })
-                const start = Date.now()
-                const timer = setInterval(() => {
-                  if (state.convertedImages[index - 1].status !== "loading") {
-                    clearInterval(timer)
-                    return
-                  }
-                  state.convertedImages[index - 1].duration = Date.now() - start
-                }, 300)
-                try {
-                  const converted = await instance.convert(
-                    new Uint8Array(await file.arrayBuffer()),
-                    targetFormat,
-                    quanlity,
-                  )
-
-                  let blobType
-
-                  if (targetFormat === MagickFormat.Pdf) {
-                    blobType = "application/pdf"
-                  } else if (targetFormat === MagickFormat.Psd) {
-                    blobType = "application/psd"
-                  } else if (targetFormat === MagickFormat.WebP) {
-                    blobType = "image/webp"
-                  }
-
-                  const blob = new Blob([converted], {
-                    type: blobType,
-                  })
-                  const previewUrl = URL.createObjectURL(blob)
-                  // state.convertedImages[index - 1].previewUrl = previewUrl
-                  state.convertedImages[index - 1].previewUrl = previewUrl
-                  state.convertedImages[index - 1].convertedSize = blob.size
-                  state.convertedImages[index - 1].downloadUrl = URL.createObjectURL(blob)
-                  state.convertedImages[index - 1].sizeChange =
-                    ((file.size - blob.size) / file.size) * 100
-                  state.convertedImages[index - 1].status = "done"
-                  state.convertedImages[index - 1].format = targetFormat
-                  state.convertedImages[index - 1].duration = Date.now() - start
-                } catch (e) {
-                  console.log("Error:", e)
-                  state.convertedImages[index - 1].status = "error"
-                }
-              })()
-            }
+        <DropZone
+          className={`w-full flex flex-col items-center justify-center drop-target:scale-125 transition-all`}
+          onDrop={async (e) => {
+            const files = e.items.filter((file) => file.kind === "file") as FileDropItem[]
+            onFiles(await Promise.all(files.map((file) => file.getFile())))
           }}
-          acceptedFileTypes={["image/*"]}
         >
-          <Button className="appearance-none inline-flex hover:shadow-2xl transition-all duration-300 hover:scale-105 dragging:bg-gray-500 items-center group space-x-2.5 bg-black text-white py-10 px-12 rounded-2xl cursor-pointer w-fit text-xl">
-            Choose file or drag here
-          </Button>
-          <p className="text-sm text-gray-500">
-            Images are not uploaded to the server, they are converted directly in your browser.
-          </p>
-        </FileTrigger>
-        <div className="flex gap-2 flex-col mt-10">
+          <FileTrigger
+            allowsMultiple
+            // acceptDirectory
+            onSelect={async (e) => {
+              if (!e) {
+                return
+              }
+              let files = Array.from(e)
+              if (files.length === 0) {
+                return
+              }
+              onFiles(files)
+            }}
+            acceptedFileTypes={["image/*"]}
+          >
+            <Button className="appearance-none inline-flex hover:shadow-2xl transition-all duration-300 hover:scale-110 dragging:bg-gray-500 items-center group space-x-2.5 bg-black text-white py-10 px-12 rounded-2xl cursor-pointer w-fit text-xl">
+              Choose file or drag here
+            </Button>
+          </FileTrigger>
+        </DropZone>
+        <p className="text-sm text-gray-500 mt-4">
+          Images are not uploaded to the server, they are converted directly in your browser.
+        </p>
+        <div className="flex gap-2 flex-col mt-10 mb-10">
           <h5>Select new format: </h5>
           <select
             value={targetFormat}
@@ -172,9 +185,9 @@ const Converter = () => {
             className="w-full p-4 border-2 border-gray-300 rounded-lg shadow-sm appearance-none"
           >
             {/* <option value={MagickFormat.WebP}>WebP</option> */}
+            <option value={MagickFormat.Png}>PNG</option>
             <option value={MagickFormat.Jpeg}>JPEG</option>
             {/* <option value={MagickFormat.WebP}>WebP</option> */}
-            <option value={MagickFormat.Png}>PNG</option>
             <option value={MagickFormat.Heic}>HEIC</option>
             <option value={MagickFormat.Bmp}>BMP</option>
             <option value={MagickFormat.Ico}>ICO</option>
